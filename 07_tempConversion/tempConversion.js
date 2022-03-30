00e75d49a2dbc610e345c5abc71a458fd2c7c2a6
INll <waileungsunny@gmail.com>
2022-03-31 01:58:33 +0800

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let output = Number((temp - 32) * (5 / 9));
+  if (Number.isSafeInteger(output)) {
+    return Math.round(output);
+  } else {
+    return Number(output.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let output = Number((temp * (9 / 5) + 32))
+  if (Number.isSafeInteger(output)) {
+    return Math.round(output);
+  } else {
+    return Number(output.toFixed(1));
+  }
 };
 
 // Do not edit below this line
