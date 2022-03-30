21decf08c656832b5b296aa63ee4c439bc3bf917
INll <waileungsunny@gmail.com>
2022-03-30 16:19:20 +0800

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, rep) {
+  if (rep <0) {
+    return 'ERROR';
+  } else {
+    let output = '';
+    for (let i = 0; i < rep; i++) {
+      output += str;
+    }
+    return output;
+  }
 };
 
 // Do not edit below this line
