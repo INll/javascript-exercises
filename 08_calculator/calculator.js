404758b0a9e6c3413f3255fac0ae4992dd99f04a
INll <waileungsunny@gmail.com>
2022-03-31 02:23:16 +0800

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return Math.max(num1, num2) - Math.min(num1, num2);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let output = 0;
+  for (let element of arr) {
+    output += element;
+  }
+  return output;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let output = 1;
+  for (let element of arr) {
+    output *= element;
+  }
+  return output;
 };
 
-const power = function() {
-	
+const power = function(num, raiseTo) {
+	let output = num;
+  for (let i = 0; i < raiseTo - 1; i++) {
+    output *= num;
+  }
+  return output;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let output = 1;
+  for (let i = 1; i <= num; i++) {
+    output *= i
+  }
+  return output;
 };
 
 // Do not edit below this line
