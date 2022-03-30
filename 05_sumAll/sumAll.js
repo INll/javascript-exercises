19785974a40bda6b7cc9b51983991e3832bde233
INll <waileungsunny@gmail.com>
2022-03-30 21:52:16 +0800

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+  if (typeof int1 != "number" || 
+      typeof int2 != "number" ||
+      int1 < 0 || int2 < 0 ) {
+    return "ERROR";
+  } else {
+    let sum = 0;
+    diff = Math.abs(int2 - int1);
+    for (let i = Math.min(int1, int2); 
+        i <= diff + 1; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
