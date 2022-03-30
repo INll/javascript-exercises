83824704e356c4eb8d5f37a36d1cdce291cdecb4
INll <waileungsunny@gmail.com>
2022-03-30 16:57:12 +0800

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let output = '';
+  for (let i = str.length - 1; i > -1; i--) {
+    output += str[i];
+  }
+  return output;
 };
 
 // Do not edit below this line
