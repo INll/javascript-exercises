fc5af894f46218487bacca7aa3746d48c21f7f0a
INll <waileungsunny@gmail.com>
2022-03-30 21:38:04 +0800

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  let output = arr;
+  for (i = 0; i < args.length; i++){
+    output = output.filter(x => x !== args[i])
+  }
+  return output;
 };
 
 // Do not edit below this line
