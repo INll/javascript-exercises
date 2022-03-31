cdec0e36bc07753429b10bf0b0e9883ac3765de9
INll <waileungsunny@gmail.com>
2022-03-31 17:55:16 +0800

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldestPersonName = '';
+  people.reduce((oldestAge, currObj) => {
+    if (currObj.yearOfDeath === undefined) {
+      if (new Date().getFullYear() - currObj.yearOfBirth > oldestAge) {
+        oldestPersonName = currObj.name;
+        return new Date().getFullYear() - currObj.yearOfBirth; 
+      }
+    } else if (currObj.yearOfDeath - currObj.yearOfBirth > oldestAge) {
+      oldestPersonName = currObj.name;
+      return currObj.yearOfDeath - currObj.yearOfBirth;
+    }
+    return oldestAge;
+  }, 0)
+  return people.find(e => e.name === oldestPersonName);
 };
 
 // Do not edit below this line
