0743dd72d3878219ecdcbb6c2910589e3e8e184d
INll <waileungsunny@gmail.com>
2022-03-31 20:46:52 +0800

    Complete 13_caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,45 @@
-const caesar = function() {
+const caesar = function(str, dist) {
+  let outputStr = '';
 
-};
+  for (let i = 0; i < str.length; i++) {
+    switch (str[i]) {
+      // Preserve punctuations
+      case ' ':
+      case ',':
+      case '!':
+      case '.':
+        outputStr += str[i];
+        continue;
+      default:
+        // Handle large numbers
+        if (dist > 26 || dist < -26) {
+          dist %= 26;
+        }
+        let originalVal = str.charCodeAt(i)
+        let shiftedVal = originalVal + dist;
+        // Handle overflow and underflow
+        if (originalVal >= 65 && originalVal <= 90) {
+          if (shiftedVal > 90) {
+            shiftedVal -= 26;
+          } else if (shiftedVal < 65) {
+            shiftedVal += 26;
+          }
+          outputStr += String.fromCharCode(shiftedVal);
+          continue;
+        }
+        if (originalVal >= 90 && originalVal <= 122) {
+          if (shiftedVal > 122) {
+            shiftedVal -= 26;
+          } else if (shiftedVal < 90) {
+            shiftedVal += 26;
+          }
+          outputStr += String.fromCharCode(shiftedVal);
+          continue;
+        }
+      }
+    }
+    return outputStr;
+  };
 
 // Do not edit below this line
 module.exports = caesar;
