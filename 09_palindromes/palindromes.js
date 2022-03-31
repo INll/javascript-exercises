e6359f484274c2a2ffbcd447abded3d85fe979e4
INll <waileungsunny@gmail.com>
2022-03-31 12:56:03 +0800

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,37 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let processed = '';
+  for (let i = 0; i < str.length; i++) {
+    switch (str[i]) {
+      case ' ':
+      case ',':
+      case '.':
+      case '!':
+        continue;
+      default:
+        processed += str[i];
+    }
+  }
+  processed = processed.toLowerCase();
 
+  let reversed = '';
+  for (let j = str.length - 1; j >= 0 ; j--) {
+    switch (str[j]) {
+      case ' ':
+      case ',':
+      case '.':
+      case '!':
+        continue;
+      default:
+        reversed += str[j];
+    }
+  }
+  reversed = reversed.toLowerCase();
+
+  if (reversed === processed) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
