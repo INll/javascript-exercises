26eed4086c85fe767b1bef93306191e6f2668232
INll <waileungsunny@gmail.com>
2022-03-31 13:09:01 +0800

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (num < 0) {
+    return 'OOPS';
+  }
+  let output = 1;
+  let prev = 0;
+  let curr = 1;
 
+  for (let i = 0; i < Number(num) - 1; i++) {
+    output = prev + curr;
+    prev = curr;
+    curr = output;
+  }
+  return output;
 };
 
 // Do not edit below this line
